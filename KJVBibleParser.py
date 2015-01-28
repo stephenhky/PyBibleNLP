b5a5b91886b7626cc4e0a1eb094f238b0efc919b
stephenhky <cwin31>
2015-01-27 22:43:37 -0500

    parse file successfully

--- KJVBibleParser.py
+++ KJVBibleParser.py
@@ -1,9 +1,106 @@
 __author__ = 'hok1'
 
 import BookAbbrDict as abbr
-import re
+from collections import defaultdict
+import re, sys
 
-ChapIndicatorRE = "CHAPTER [1-9][0-9]|CHAPTER [0-9]|PSALM [1-9][1-9][0-9]|PSALM [1-9][0-9]|PSALM [0-9]"
+bknameref = {
+    "Genesis": "gen",
+    "Exodus": "exo",
+    "Leviticus": "lev",
+    "Numbers": "num",
+    "Deuteronomy": "deu",
+    "Joshua": "jos",
+    "Judges": "jdg",
+    "Ruth": "rut",
+    "1 Samuel": "sa1",
+    "2 Samuel": "sa2",
+    "1 Kings": "kg1",
+    "2 Kings": "kg2",
+    "1 Chronicles": "ch1",
+    "2 Chronicles": "ch2",
+    "Ezra": "ezr",
+    "Nehemiah": "neh",
+    "Esther": "est",
+    "Job": "job",
+    "Psalms": "psa",
+    "Proverbs": "pro",
+    "Ecclesiastes": "ecc",
+    "Song of Solomon": "sol",
+    "Isaiah": "isa",
+    "Jeremiah": "jer",
+    "Lamentations": "lam",
+    "Ezekiel": "eze",
+    "Daniel": "dan",
+    "Hosea": "hos",
+    "Joel": "joe",
+    "Amos": "amo",
+    "Obadiah": "oba",
+    "Jonah": "jon",
+    "Micah": "mic",
+    "Nahum": "nah",
+    "Habakkuk": "hab",
+    "Zephaniah": "zep",
+    "Haggai": "hag",
+    "Zechariah": "zac",
+    "Malachi": "mal",
+    "1 Esdras": "es1",
+    "2 Esdras": "es2",
+    "Tobias": "tob",
+    "Judith": "jdt",
+    "Additions to Esther": "aes",
+    "Wisdom": "wis",
+    "Baruch": "bar",
+    "Epistle of Jeremiah": "epj",
+    "Susanna": "sus",
+    "Bel and the Dragon": "bel",
+    "Prayer of Manasseh": "man",
+    "1 Macabees": "ma1",
+    "2 Macabees": "ma2",
+    "3 Macabees": "ma3",
+    "4 Macabees": "ma4",
+    "Sirach": "sir",
+    "Prayer of Azariah": "aza",
+    "Laodiceans": "lao",
+    "Joshua B": "jsb",
+    "Joshua A": "jsa",
+    "Judges B": "jdb",
+    "Judges A": "jda",
+    "Tobit BA": "toa",
+    "Tobit S": "tos",
+    "Psalms of Solomon": "pss",
+    "Bel and the Dragon Th": "bet",
+    "Daniel Th": "dat",
+    "Susanna Th": "sut",
+    "Odes": "ode",
+    "Matthew": "mat",
+    "Mark": "mar",
+    "Luke": "luk",
+    "John": "joh",
+    "Acts": "act",
+    "Romans": "rom",
+    "1 Corinthians": "co1",
+    "2 Corinthians": "co2",
+    "Galatians": "gal",
+    "Ephesians": "eph",
+    "Philippians": "phi",
+    "Colossians": "col",
+    "1 Thessalonians": "th1",
+    "2 Thessalonians": "th2",
+    "1 Timothy": "ti1",
+    "2 Timothy": "ti2",
+    "Titus": "tit",
+    "Philemon": "plm",
+    "Hebrews": "heb",
+    "James": "jam",
+    "1 Peter": "pe1",
+    "2 Peter": "pe2",
+    "1 John": "jo1",
+    "2 John": "jo2",
+    "3 John": "jo3",
+    "Jude": "jde",
+    "Revelation": "rev"
+}
 
 class KJVParser:
     def __init__(self, bookdir):
@@ -12,18 +109,30 @@ class KJVParser:
         self.bookcontent = {}
 
     def parseBook(self, bookabbr):
-        basefilename = abbr.getBookName(bookabbr)+'.txt'
-        filepath = self.bookdir + '/' + basefilename
+        filepath = self.bookdir+'/'+'kjvdat.txt'
         self.currentbook = bookabbr
-        self.bookcontent = {}
+        self.bookcontent = defaultdict(lambda : {})
+        bookname = abbr.getBookName(bookabbr)
+        datbookname = bknameref[bookname]
 
-        fin = open(filepath, 'rb')
-        fin.readline()
-        fin.readline()
-        chapIdx = 0
-        verseIdx = 0
-        for line in fin.readline():
-            line = line.strip()
-            re.match(ChapIndicatorRE, line)
-            firstSpacePos = line.find(' ')
+        bibdat = open(filepath, 'rb')
+        for line in bibdat:
+            bkname, chap, verse, scripture = line.split('|')
+            if bkname.lower()==datbookname.lower():
+                chap = int(chap)
+                verse = int(verse)
+                scripture = scripture.strip()
+                self.bookcontent[chap][verse] = scripture[:-1] if re.search('~$', scripture) else scripture
+        bibdat.close()
+        self.bookcontent = dict(self.bookcontent)
+
+    def retrieveVerse(self, bookabbr, chap, verse):
+        if bookabbr != self.currentbook:
+            self.parseBook(bookabbr)
+        try:
+            scripture = self.bookcontent[chap][verse]
+        except KeyError:
+            sys.stderr.write('No verse: '+bookabbr+' '+str(chap)+':'+str(verse)+'\n')
+            scripture = ''
+        return scripture
 
