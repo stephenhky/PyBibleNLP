ebc577a54c2a4546f71f25deb78bbbfcb0927742
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2015-01-30 11:34:01 -0500

    encapsulate exception message appropriately

--- BibleExceptions.py
+++ BibleExceptions.py
@@ -2,16 +2,22 @@ __author__ = 'hok1'
 
 class BibleException(Exception):
     def __init__(self, msg):
-        self.message = msg
+        self._message = msg
+
+    def _get_message(self):
+        return self._message
+
+    def __str__(self):
+        return self._get_message()
 
 class NoBibleBookException(BibleException):
     def __init__(self, bookabbr):
-        self.message = 'No such book ('+bookabbr+') exists.'
+        self._message = 'No such book ('+bookabbr+') exists.'
 
 class InvalidBibleLocationException(BibleException):
     def __init__(self, bookabbr, chap, verse):
-        self.message = 'No such location '+bookabbr+' '+str(chap)+':'+str(verse)
+        self._message = 'No such location '+bookabbr+' '+str(chap)+':'+str(verse)
 
 class InvalidBibleChapterException(BibleException):
     def __init__(self, bookabbr, chap):
-        self.message = 'The book '+bookabbr+' does not have chapter '+str(chap)
+        self._message = 'The book '+bookabbr+' does not have chapter '+str(chap)
