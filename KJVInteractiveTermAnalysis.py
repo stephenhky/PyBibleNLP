476b83bbcef0fecec8af038a46a34aa4e3573bc0
stephenhky <cwin31>
2015-03-05 17:33:24 -0500

    interactive process separated

---
+++ KJVInteractiveTermAnalysis.py
@@ -0,0 +1,24 @@
+__author__ = 'stephenhky'
+
+import BookAbbrDict as abbr
+from scipy.spatial.distance import cosine
+import argparse
+import numpy as np
+from BibleVectorsAnalysis import BibleBookAnalysis
+
+def performLSIAnalysis(npz_dir):
+    analyzer = BibleBookAnalysis(npz_dir)
+    termdocMatrix = analyzer.getTermBookMatrix(abbr.otbookdict.keys()+abbr.ntbookdict.keys())
+    U, s, V = np.linalg.svd(termdocMatrix, full_matrices=False)
+    while True:
+        idx = int(raw_input('Index = ? '))
+        relevances = [(token, 1-cosine(U[:, idx], analyzer.tokenVecs[token])) for token in analyzer.tokenVecs.keys()]
+        relevances = sorted(relevances, key=lambda item: item[1], reverse=True)
+        for relevance in relevances[:30]:
+            print relevance
+
+if __name__ == '__main__':
+    argv_parser = argparse.ArgumentParser(description='Analyzing bible books')
+    argv_parser.add_argument('npz_dir', help='token information directory')
+    args = argv_parser.parse_args()
+    performLSIAnalysis(args.npz_dir)
