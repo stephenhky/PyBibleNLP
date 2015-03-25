6697a51e996922e5a542a186bee99789e5372efc
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2015-03-25 12:03:21 -0400

    LDA implementation complete

--- analytics/lda/LDA.py
+++ analytics/lda/LDA.py
@@ -3,6 +3,7 @@ __author__ = 'hok1'
 import numpy as np
 from gensim.models.ldamodel import LdaModel
 from gensim import corpora
+import vectorize.DocVectorization as dv
 
 class LatentDirichletAllocation:
     def __init__(self, num_topics=50, corpus=None, dictionary=None, stemfunc=lambda s: s):
@@ -10,6 +11,7 @@ class LatentDirichletAllocation:
         self.corpus = corpus
         self.dictionary = dictionary
         self.stemfunc = stemfunc
+        self.vectorizer = dv.DocVectorizer()
 
     def loadCorpus(self, mmfile, dictfile):
         self.corpus = corpora.MmCorpus(mmfile)
@@ -17,3 +19,13 @@ class LatentDirichletAllocation:
 
     def trainLDAmodel(self):
         self.lda = LdaModel(self.corpus, num_topics=self.num_topics)
+
+    def queryDocTopics(self, docstr):
+        return self.lda[ self.dictionary.doc2bow( self.vectorizer.tokenizeDoc(docstr))]
+
+    def stringTopic(self, topicid):
+        return map(lambda weighttoken: (weighttoken[0], self.dictionary[int(weighttoken[1])]),
+                   self.lda.show_topic(topicid))
+
+    def numTopics(self):
+        return self.lda.num_topics
