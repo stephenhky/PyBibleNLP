5608f8ccd615e4b8246b9d85e39ea107b7397b34
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2015-04-07 14:11:15 -0400

    ESV scraper done

---
+++ Bible/ESV/ESVOnlineBibleParser.py
@@ -0,0 +1,22 @@
+__author__ = 'hok1'
+
+import Bible.BookAbbrDict as abbr
+import urllib
+import urllib2
+
+baseurl = 'http://www.esvapi.org/v2/rest/'
+
+class ESVOnlineParser:
+    def __init__(self, key):
+        self.key = key
+
+    def query(self, bookabbr, chapidx):
+        bookname = abbr.getBookName(bookabbr)
+        query_args = {'key': self.key, 'passage': bookname+' '+str(chapidx)}
+        query_url = baseurl+'passageQuery?'+urllib.urlencode(query_args)
+        return urllib2.urlopen(query_url)
+
+    def save_url(self, path, response):
+        fout = open(path, 'wb')
+        fout.writelines(response.readlines())
+        fout.close()
---
+++ ESVBibleOnlineScraper.py
@@ -0,0 +1,27 @@
+__author__ = 'hok1'
+
+import Bible.BookAbbrDict as abbr
+import Bible.ESV.ESVOnlineBibleParser as esvparse
+import argparse
+import pickle
+
+def getArgParser():
+    argparser = argparse.ArgumentParser(description='Parse ESV Bible from API and save it')
+    argparser.add_argument('key', help='Key')
+    argparser.add_argument('output_path', help='Output Path')
+    argparser.add_argument('chapkey', help='Path of chapkey.pickle')
+    return argparser
+
+if __name__ == '__main__':
+    argparser = getArgParser()
+    args = argparser.parse_args()
+    output_dir = args.output_path
+    esvkey = args.key
+
+    parser = esvparse.ESVOnlineParser(esvkey)
+
+    for bookabbr, chapidx in pickle.load(open(args.chapkey, 'rb')):
+        print 'Retrieving '+abbr.getBookName(bookabbr)+' '+str(chapidx)
+        response = parser.query(bookabbr, chapidx)
+        print '\tWriting...'
+        parser.save_url(output_dir+'/'+bookabbr+'_'+str(chapidx)+'.html', response)
