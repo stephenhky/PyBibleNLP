81be4fb948253d307bfd3487da0f434b0d388ee3
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2015-04-22 14:38:08 -0400

    reducing the amount of characters in codes

--- analytics/lda/LDA.py
+++ analytics/lda/LDA.py
@@ -2,23 +2,23 @@ __author__ = 'hok1'
 
 from gensim.models.ldamodel import LdaModel
 from analytics.topicmodels import TopicModeler
-from gensim import similarities
+from gensim.similarities import MatrixSimilarity
 
 class LatentDirichletAllocation(TopicModeler):
     def trainModel(self):
         if self.toweight:
             self.model = LdaModel(self.tfidf[self.corpus], num_topics=self.num_topics)
-            self.index = similarities.MatrixSimilarity(self.model[self.tfidf[self.corpus]])
+            self.index = MatrixSimilarity(self.model[self.tfidf[self.corpus]])
         else:
             self.model = LdaModel(self.corpus, num_topics=self.num_topics)
-            self.index = similarities.MatrixSimilarity(self.model[self.corpus])
+            self.index = MatrixSimilarity(self.model[self.corpus])
 
     def loadModel(self, modelfile):
         self.model = LdaModel.load(modelfile)
         if self.toweight:
-            self.index = similarities.MatrixSimilarity(self.model[self.tfidf[self.corpus]])
+            self.index = MatrixSimilarity(self.model[self.tfidf[self.corpus]])
         else:
-            self.index = similarities.MatrixSimilarity(self.model[self.corpus])
+            self.index = MatrixSimilarity(self.model[self.corpus])
 
     def queryDocTopics(self, docstr):
         if self.toweight:
--- analytics/lsi/LSI.py
+++ analytics/lsi/LSI.py
@@ -1,24 +1,24 @@
 __author__ = 'stephenhky'
 
 from gensim.models import LsiModel
-from gensim import similarities
+from gensim.similarities import MatrixSimilarity
 from analytics.topicmodels import TopicModeler
 
 class LatentSemanticIndexing(TopicModeler):
     def trainModel(self):
         if self.toweight:
             self.model = LsiModel(self.tfidf[self.corpus], num_topics=self.num_topics)
-            self.index = similarities.MatrixSimilarity(self.model[self.tfidf[self.corpus]])
+            self.index = MatrixSimilarity(self.model[self.tfidf[self.corpus]])
         else:
             self.model = LsiModel(self.corpus, num_topics=self.num_topics)
-            self.index = similarities.MatrixSimilarity(self.model[self.corpus])
+            self.index = MatrixSimilarity(self.model[self.corpus])
 
     def loadModel(self, modelfile):
         self.model = LsiModel.load(modelfile)
         if self.toweight:
-            self.index = similarities.MatrixSimilarity(self.model[self.tfidf[self.corpus]])
+            self.index = MatrixSimilarity(self.model[self.tfidf[self.corpus]])
         else:
-            self.index = similarities.MatrixSimilarity(self.model[self.corpus])
+            self.index = MatrixSimilarity(self.model[self.corpus])
 
 
     def __str__(self):
