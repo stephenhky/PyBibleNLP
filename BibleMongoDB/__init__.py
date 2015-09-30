b0c1884e32c814d8c4e3857873eb16e5c247030c
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2015-09-30 17:08:32 -0400

    deleted irrelevant codes, getting ready to store bibles in sqlite database

--- BibleMongoDB/__init__.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'hok1'
-
-# remember to run " mongod --config /usr/local/etc/mongod.conf " before using MongoDB package
-
-from pymongo import MongoClient
-
-dbname = 'PyBibleNLP'
-client = MongoClient()
-bibledb = client[dbname]
--- vectorize/DocVectorization.py
+++ vectorize/DocVectorization.py
@@ -1,26 +1,16 @@
 __author__ = 'hok1'
 
-from gensim.models import word2vec
 from gensim import corpora
 from nltk.tokenize import TreebankWordTokenizer
 from nltk.corpus import stopwords
 import re
-import numpy as np
 
 class DocVectorizer:
-    def __init__(self, modelfilename=None, binary=True, toRemoveDigits=True, toLower=True, toRemoveStopWords=True):
-        if modelfilename != None:
-            self.loadmodel(modelfilename, binary=binary)
+    def __init__(self, toRemoveDigits=True, toLower=True, toRemoveStopWords=True):
         self.toRemoveDigits = toRemoveDigits
         self.toLower = toLower
         self.toRemoveStopWords = toRemoveStopWords
         self.tokenizer = TreebankWordTokenizer()
-
-    def loadmodel(self, modelfilename, binary=True):
-        self.wmodel = word2vec.Word2Vec.load_word2vec_format(modelfilename, binary=binary)
-
-    def unloadmodel(self):
-        self.wmodel = None
 
     def tokenizeDoc(self, docstr):
         tokenizedDoc = self.tokenizer.tokenize(docstr)
@@ -33,14 +23,6 @@ class DocVectorizer:
         if self.toRemoveStopWords:
             tokenizedDoc = filter(lambda s: not (s.lower() in stopwords.words()), tokenizedDoc)
         return tokenizedDoc
-
-    def retrieveWord2VecDocVectors(self, docstr):
-        tokens = self.tokenizeDoc(docstr)
-        vectors = map(lambda token: self.wmodel[token] if (token in self.wmodel) else None, tokens)
-        filteredPairs = filter(lambda pair: pair[1]!=None, zip(tokens, vectors))
-        tokens = map(lambda pair: pair[0], filteredPairs)
-        vectors = np.array(map(lambda pair: pair[1], filteredPairs))
-        return tokens, vectors
 
     def retrieveGensimCorpora(self, docs, stemfunc=lambda s: s):
         texts = map(self.tokenizeDoc, docs)
